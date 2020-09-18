ba4c319ae358fc475aa30542ed9a41225d600c1c
julio hernandez <julioandres72@gmail.com>
2020-09-18 15:50:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@
 def square_array(array)
   counter = 0
   length = array.length
-  while counter < lenght do
+  while counter < length do
     array[counter] = array[counter] ** 2
     counter += 1
   end
